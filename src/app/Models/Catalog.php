5a844d3009ff2367704ee2fac6c54cf2a6ddac35
Eugene Che <indexpro24@gmail.com>
2024-07-13 21:32:34 +0300

    c

---
+++ src/app/Models/Catalog.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+
+class Catalog extends RootModel
+{
+protected $table = 'catalog';
+
+    // public function rubric(): MorphMany
+    // {
+    //     return $this->morphMany(Rubric::class, 'parentable');
+    // }
+
+    // public function text(): MorphMany
+    // {
+    //     return $this->morphMany(Text::class, 'parentable');
+    // }
+
+    // public function parentable(): MorphTo
+    // {
+    //     return $this->morphTo();
+    // }
+}
